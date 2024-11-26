a0baefd0951fa68570610cefa82d43f03da647b6
abdellziz <aitouane.abdelaziz@gmail.com>
2024-11-26 22:42:23 +0100

    firs commit

---
+++ first.py
@@ -0,0 +1,9 @@
+for name in range(0, 51):
+        print("abdellaziz")
+
+for i in range(1000, 9555, 10):
+        print(i)
+
+name = "aitouane abdellaziz"
+for i in range(len(name)):
+    print(name[i], end=" ")
